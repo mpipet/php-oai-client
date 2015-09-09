1bb0d81bdbfbf93ea2a9856855a67cc673472b26
mpipet <contact@mathieupipet.fr>
2015-09-09 19:55:19 +0200

    supression var_dump

--- OaiClient.php
+++ OaiClient.php
@@ -30,7 +30,6 @@ class OaiClient
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        var_dump($url);
         $xml = curl_exec($curl);
 
         if (curl_error($curl) || $xml === '') {
